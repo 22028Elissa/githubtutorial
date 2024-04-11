11c989ffb62f4d65e144fdc2dfed2b2d5fc0c826
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-12 09:58:21 +1200

    printed all aircraft

--- Database example/app.py
+++ Database example/app.py
@@ -1,9 +1,25 @@
+#docstring- Elissa Piao- Airplance batabse application
+#import
 import sqlite3
 
-db = sqlite3.connect("fighters.db")
-cursor = db.cursor()
-sql = "SELECT * FROM fighter;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+#Contants and variables
+DATABASE = "fighters.db"
+
+
+#functions
+def print_all_aircraft():
+    '''print all the aircraft nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    for fighter in results:
+        print(fighter)
+        #loop finishes here
+    db.close()
+
+
+#main code
+print_all_aircraft()
