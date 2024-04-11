97c6c8645d314c442926fb89d8cfb3a19e8d834d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-12 10:20:52 +1200

    made code for asking user input

--- Database example/app.py
+++ Database example/app.py
@@ -15,11 +15,23 @@ def print_all_aircraft():
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
+    print(f"name                          speed   max_g climb range payload")
     for fighter in results:
-            print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<8}{fighter[4]:<8}{fighter[5]:<8}{fighter[6]:<8}")
+            print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
         #loop finishes here
     db.close()
 
 
 #main code
-print_all_aircraft()
+while True:
+    user_input = input("\nWhat would you like to do?. \n1. Print all aircraft\n7. Exit\n")
+    if user_input == "1":
+        print_all_aircraft()
+    if user_input == "2":
+        pass
+    if user_input == "3":
+        pass
+    if user_input == "7":
+        break
+    else:
+        print("\nThat was not an option\n")
